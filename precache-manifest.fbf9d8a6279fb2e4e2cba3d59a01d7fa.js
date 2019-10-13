7c7d7bc23a6468c62f070c1c96e377929321e53d
kola <kola@kola.com>
2019-10-14 03:18:52 +0530

    first commit

---
+++ precache-manifest.fbf9d8a6279fb2e4e2cba3d59a01d7fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0910ce8c7093650982a926b2c2c59808",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed1627ad244e3242ffbb",
+    "url": "/static/css/main.7307fa26.chunk.css"
+  },
+  {
+    "revision": "e22fa30d49a1fdfb76c6",
+    "url": "/static/js/2.45fbcd57.chunk.js"
+  },
+  {
+    "revision": "ed1627ad244e3242ffbb",
+    "url": "/static/js/main.691d65ea.chunk.js"
+  },
+  {
+    "revision": "2daf99910e09e95c46a1",
+    "url": "/static/js/runtime~main.4198b842.js"
+  }
+]);
